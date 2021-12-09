aea95811fe21c03221ef149ebb1cb8fb2a5325e2
Victor Souza <victor.augusto800@gmail.com>
2021-12-09 10:56:25 -0300

    Adicionar funcionabilidade de cadastro

---
+++ index.js
@@ -0,0 +1,20 @@
+const btnCadastro = document.getElementById("btnCadastro");
+
+const form = document.getElementById("form");
+
+const cadastro = (e) => {
+  e.preventDefault();
+
+  localStorage.setItem(
+    localStorage.length + 1,
+    JSON.stringify({
+      name: form.elements[0].value,
+      cpf: form.elements[1].value,
+      tel: form.elements[2].value,
+      email: form.elements[3].value,
+    })
+  );
+  form.reset();
+};
+
+form.addEventListener("submit", cadastro, true);
